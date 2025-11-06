4e4985db7d27ca3209fe60bbce3f11182555ed33
Tim <schreyack@yahoo.com>
2025-11-05 16:25:21 -0800

    Remove winners from the player pool to prevent them from winning again

--- server.py
+++ server.py
@@ -251,6 +251,10 @@ def select_winner():
                 winners_list.append(winner)
                 save_winners_list(winners_list)
                 save_winner([winner])
+                # Remove winner from the pool to prevent them from winning again
+                if winner in names_dict:
+                    del names_dict[winner]
+                    save_names(names_dict)
                 global_chances = load_chances()
                 if global_chances > 0:
                     save_chances(global_chances - 1)
